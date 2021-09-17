56d148e9f44b16592451f5e76e0e79315be59bc2
Michael Niksa <miniksa@microsoft.com>
2021-09-17 15:31:43 -0700

    code format!

--- src/host/VtIo.cpp
+++ src/host/VtIo.cpp
@@ -160,7 +160,7 @@ VtIo::VtIo() :
             case VtIoMode::XTERM_256:
             {
                 auto xterm256Engine = std::make_unique<Xterm256Engine>(std::move(_hOutput),
-                                                                    initialViewport);
+                                                                       initialViewport);
                 if (_passthroughMode)
                 {
                     auto vtapi = new VtApiRoutines();
